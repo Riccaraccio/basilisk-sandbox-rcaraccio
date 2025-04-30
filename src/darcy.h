493920012558773faa98b4b2100cd921f5cb136e
Riccaraccio <riccardo.caraccio@polimi.it>
2025-04-30 11:57:01 +0200

    Refactor viscous term calculations in Darcy flow implementation for improved clarity and performance

--- src/darcy.h
+++ src/darcy.h
@@ -19,29 +19,17 @@ double Da = 5e-3; //to be chaged to coord Da
 
 event viscous_term (i++) {
   correction (dt);
-  // foreach() {
-  //   if (f[] > F_ERR) {
-  //     double e = porosity[]/f[];
-  //     double F = 1.75/pow (150*pow (e, 3), 0.5);
-
-  //     foreach_dimension() {
-  //       double A = alpha.x[]/(fm.x[] + SEPS)*(muG*e/Da)*f[]; //note: porosity[] = e[]*f[] here
-  //       double B = alpha.x[]/(fm.x[] + SEPS)*(F*e*rhoG/pow(Da,0.5))*fabs(u.x[])*f[];
-
-  //       u.x[] /= (1. + (A+B)*dt);
-  //     }
-  //   }
-  // }
   foreach() {
     if (f[] > F_ERR) {
       double e = porosity[]/f[];
       double F = 1.75/pow (150*pow (e, 3), 0.5);
+      double Umag = sqrt(sq(u.x[]) + sq(u.y[]));
 
       foreach_dimension() {
-        double A = alpha.x[]/(fm.x[] + SEPS)*(muG*e/Da)*f[];
-        double B = alpha.x[]/(fm.x[] + SEPS)*(F*e*rhoG/pow(Da,0.5))*f[];
+        double A = alpha.x[]/(fm.x[] + SEPS)*(muG*e/Da)*f[]; 
+        double B = alpha.x[]/(fm.x[] + SEPS)*(F*e*rhoG/pow(Da,0.5))*Umag*f[];
 
-        u.x[] = (-(1+A*dt) + sqrt(sq(1+A*dt) + 4*fabs(u.x[])*B*dt))/(2*B*dt)*sign(u.x[]);
+        u.x[] /= (1. + (A+B)*dt);
       }
     }
   }

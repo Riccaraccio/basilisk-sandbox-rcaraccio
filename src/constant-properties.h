9d70931056acbe6d16eb53ffd98de899f6834f3a
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2025-11-11 15:44:38 +0100

    Added doc

--- src/constant-properties.h
+++ src/constant-properties.h
@@ -1,3 +1,14 @@
+/**
+# Constant properties
+This file contains functions to update physical properties in simulations 
+of two-phase flow with phase change in porous media. We want to solve the
+Navier-Stokes equations using constant properties for the gas phase throughout
+the domain, both in the gas and in the porous solid regions.
+To achieve this, we overload the `properties` event to set the density and viscosity
+of the gas also in the solid regions. Additionally, we set the thermal conductivity
+and mass diffusivity for multicomponent simulations.
+*/
+
 #ifndef F_ERR
 # define F_ERR 1.e-10
 #endif
@@ -10,6 +21,14 @@ extern face vector alphav;
 extern scalar rhov;
 extern scalar f;
 extern scalar porosity;
+
+/**
+# Thermal conductivity boundary conditions
+Given that we also want to account for different thermal conductivities
+in different directions, lambda1v and lambda2v are defined as vectors.
+This set of boundary conditions are needed to ensure the correct calculation
+of the heat flux at the boundaries.
+*/
 
 #ifdef SOLVE_TEMPERATURE
 extern vector lambda1v, lambda2v;
@@ -40,8 +59,12 @@ extern scalar* DmixGList_S;
 extern scalar* DmixGList_G;
 #endif
 
+/**
+# Update properties with constant gas properties
+This function updates the physical properties used in the Navier-Stokes equations
+to have constant gas properties throughout the domain.
+*/
 void update_properties_constant (void) {
-  //Navier-Stokes equation are solved using the gas properties on the whole domain
   foreach_face() {
     alphav.x[] = fm.x[]/rhoG;
     {
@@ -54,6 +77,10 @@ void update_properties_constant (void) {
     rhov[] = rhoG*cm[];
 
 #ifdef SOLVE_TEMPERATURE
+  /**
+  The pseudophase thermal conductivities are calcuated as weighted averages
+  based on the porosity field.
+  */
   foreach() {
     foreach_dimension() {
       lambda1v.x[] = f[] > F_ERR ? pavg (porosity[]/f[], lambdaG, lambdaS) : 0.;
@@ -73,11 +100,22 @@ void update_properties_constant (void) {
     }
     for (int jj = 0; jj < NGS; jj++) {
       scalar Dmix2 = DmixGList_S[jj];
-      Dmix2[] = f[] > F_ERR ? Dmixv * pow(porosity[], 4./3.) : 0.; // effect of solid, to be revised
+      /**
+      In the solid region, the mass diffusivity is modified to account for
+      the effect of the solid matrix.
+      The simplest model is used here, where the diffusivity is scaled by the porosity.
+      */
+      Dmix2[] = f[] > F_ERR ? Dmixv * pow(porosity[], 4./3.) : 0.;
     }
   }
 #endif
 }
+
+/**
+# Overload properties event
+Every time the properties event is called, we update the properties
+to our desired constant gas properties.
+*/
 
 event properties (i++) {
   update_properties_constant();

a8404859a7e465239265399fb24bbc6f96cec74d
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2025-11-28 16:22:15 +0100

    improved doc

--- src/superquadric.h
+++ src/superquadric.h
@@ -1,8 +1,12 @@
-// We want to define a macro that returns the levelset function of a superquadric
-// The superquadric is defined by the following equation:
-// (x/a)^m + (y/b)^m = 1
-// where m is the shape parameter, a is the radius in the x direction, and b is the radius in the y direction
-// The levelset function is defined as the distance from the surface of the superquadric
+/**
+We define a simple macro that returns the levelset function of a superquadric.
+This familiy of shapes are defined by the following equation:
+$$
+\left(\frac{x}{a}\right)^m + \left(\frac{y}{b}\right)^m = 1
+$$
+where 'm' is the shape parameter, 'a' is the radius in the x direction, and 'b' is the radius in the y direction.
+The levelset function is defined as the distance from the surface of the superquadric.
+*/
 
 macro double superquadric (double x, double y, 
                             double m, double a, double b,
@@ -10,15 +14,3 @@ macro double superquadric (double x, double y,
 {
     return 1. - pow((x-xc*L0)/a, m) - pow((y-yc*L0)/b, m);
 }
-
-// macro double rectangle (double x, double y, 
-//                     double a, double b, 
-//                     double xc = 0., double yc = 0.)
-// {
-//   double distancex = fabs(x - xc*L0) - a;
-//   double distancey = fabs(y - yc*L0) - b;
-
-//   double outside_distance = max(max(distancex, distancey), 0.); //FIXME
-//   double inside_distance = min(max(distancex, distancey), 0.);
-//   return outside_distance + inside_distance;
-// }

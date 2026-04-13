4ae1fdcbf63e95d920a803ab0be6760c866b9f5c
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2026-04-13 11:33:08 +0200

    updated testcase

--- run/pah.c
+++ run/pah.c
@@ -4,6 +4,7 @@
 #define FICK_CORRECTED 1
 #define MASS_DIFFUSION_ENTHALPY 1
 #define TURN_OFF_HEAT_OF_REACTION 1
+#define GAS_PHASE_REACTIONS 1
 
 #include "axi.h"
 #include "navier-stokes/centered-phasechange.h"
@@ -11,7 +12,6 @@
 #include "two-phase.h"
 #include "shrinking.h"
 #include "multicomponent-varprop.h"
-#include "darcy.h"
 #include "view.h"
 
 u.n[top]      = neumann (0.);
@@ -26,8 +26,8 @@ p[right]      = dirichlet (0.);
 pf[right]     = dirichlet (0.);
 psi[right]    = dirichlet (0.);
 
-const double tend = 10 ; //simulation time 60 s
-int maxlevel = 8; int minlevel = 2;
+const double tend = 60 ; //simulation time 60 s
+int maxlevel = 7; int minlevel = 2;
 double D0 = 2e-2;
 double solid_mass0 = 0.;
 
@@ -35,7 +35,7 @@ int main () {
     lambdaSmodel = L_LU;
     emissivity = emissivity_lu;
 
-    TS0 = 550.; TG0 = 1173.;
+    TS0 = 750.; TG0 = 1173.;
     rhoS = 1500;
     eps0 = 0.4;
 
@@ -48,6 +48,7 @@ int main () {
 
     DT = 1.e-2;
     kinfolder = "biomass/full-solid-gas";
+
     L0 = 10*D0;
     init_grid(1 << maxlevel);
 
@@ -64,14 +65,21 @@ event init (i = 0) {
 
   gas_start[OpenSMOKE_IndexOfSpecies ("N2")] = 1.;
 
-  sol_start[OpenSMOKE_IndexOfSolidSpecies ("BIOMASS")] = 1.;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("CELL")] = 0.4082;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("XYHW")] = 0.1991;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("LIGO")] = 0.0189;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("LIGH")] = 0.1536;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("LIGC")] = 0.1633;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("TANN")] = 0.0184;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("TGL")] = 0.0385;
+  sol_start[OpenSMOKE_IndexOfSolidSpecies ("MOIST")] = 0.;
 
   foreach()
     porosity[] = eps0*f[];
 
   solid_mass0 = 0.;
   foreach (reduction(+:solid_mass0))
-    solid_mass0 += (f[]-porosity[])*rhoS*dv(); //
+    solid_mass0 += (f[] - porosity[])*rhoS*dv();
 
   TG[right] = dirichlet (TG0);
   TG[top] = dirichlet (TG0);
@@ -94,15 +102,15 @@ event init (i = 0) {
 }
 
 // Prints radial profile of a scalar at y=0 location
-void print_radial_profile (scalar f, FILE* fp, int time, int n_samples = 100, const double length = L0) {
+void print_radial_profile (scalar f, FILE* fp, double time, int n_samples = 100, const double length = L0) {
   double step = length/n_samples;
   for (double xx = 0.; xx <= length; xx += step) {
     double val = interpolate (f, xx, 0.);
-    fprintf (fp, "%d %g %g\n", time, xx, val);
+    fprintf (fp, "%g %g %g\n", time, xx, val);
   }
 }
 
-event output (t += 0.1) {
+event output (t += 0.01) {
   scalar yC6H6 = YGList_G[OpenSMOKE_IndexOfSpecies ("C6H6")];
   scalar yC10H8 = YGList_G[OpenSMOKE_IndexOfSpecies ("C10H8")];
   scalar yC12H8 = YGList_G[OpenSMOKE_IndexOfSpecies ("C12H8")];
@@ -115,20 +123,29 @@ event output (t += 0.1) {
     yBIN[] = yBINA[] + yBINB[];
   }
 
-  print_radial_profile (yC6H6, fyC6H6, (int) round(t));
-  print_radial_profile (yC10H8, fyC10H8, (int) round(t));
-  print_radial_profile (yC12H8, fyC12H8, (int) round(t));
-  print_radial_profile (yC16H10, fyC16H10, (int) round(t));
-  print_radial_profile (yBIN, fyBIN, (int) round(t));
+  print_radial_profile (yC6H6, fyC6H6, t);
+  print_radial_profile (yC10H8, fyC10H8, t);
+  print_radial_profile (yC12H8, fyC12H8, t);
+  print_radial_profile (yC16H10, fyC16H10, t);
+  print_radial_profile (yBIN, fyBIN, t);
 
   fflush(fyC6H6);
   fflush(fyC10H8);
   fflush(fyC12H8);
   fflush(fyC16H10);
   fflush(fyBIN);
+
+  double solid_mass = 0.;
+  foreach (reduction(+:solid_mass))
+    solid_mass += (f[] - porosity[])*rhoS*dv();
+
+  char name[20];
+  sprintf (name, "OutputData-%d", maxlevel);
+  static FILE* fp = fopen (name, "w");
+  fprintf (fp, "%g %g\n", t, solid_mass/solid_mass0);
+  fflush(fp);
 }
 
-#if TREE
 event adapt (i++) {
   scalar yLVG = YGList_G[OpenSMOKE_IndexOfSpecies ("C6H10O5")];
   scalar yC6H6 = YGList_G[OpenSMOKE_IndexOfSpecies ("C6H6")];
@@ -136,19 +153,17 @@ event adapt (i++) {
   adapt_wavelet_leave_interface ({T, u.x, u.y, yLVG, yC6H6, porosity}, {f},
     (double[]){1.e-1, 1.e-0, 1.e-0, 1e-1, 1e-1, 1e-0}, maxlevel, minlevel, 2);
 }
-#endif
 
 event stop (t = tend) {
-  fclose(fTprofile_2mm);
-  fclose(fTprofile_11mm);
-  fclose(fxH2Oprofile_2mm);
-  fclose(fxH2Oprofile_11mm);
-  fclose(fxOHprofile_2mm);
-  fclose(fxOHprofile_11mm);
+  fclose(fyC6H6);
+  fclose(fyC10H8);
+  fclose(fyC12H8);
+  fclose(fyC16H10);
+  fclose(fyBIN);
 }
 
-event movie (t += 0.1) {
-  view (width = 800, height = 400, fov = 20);
+event movie (t += 0.01) {
+  view (tx=-0.5, ty=-0.5, width=1080, height=1080);
   clear();
   draw_vof ("f", lw = 2);
   cells();

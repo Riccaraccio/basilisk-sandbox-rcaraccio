94d5a689d585ffe9f6281721e8485cdb0858d29b
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2026-06-10 18:18:52 +0200

    new tests for load balancing

--- test/balance-rotate.c
+++ test/balance-rotate.c
@@ -1,9 +1,62 @@
 /**
 # Rotation of a circular interface - MPI Load Balancing test
+
+This test is a modified version of [rotate.c](/src/test/rotate.c).
+The goal is to test the correct split of load between processors, given a user 
+assinged scalar field 'weights', a representative of the computational load being
+carried out in a specific cell.
+This is also a twin to the test [balance-rotate-auto.c](/src/test/balance-rotate-auto.c)
+where instead the weights are detected automatically.
+
+We advect a circular interface along a circular path. To simulate an inbalance 
+in the load between cells, we add an intensive calculation only in the region
+marked by the 'c' field. 
+
+The total load is defined as the sum of the individual weights of each cells.
+Ideally, each processor gets total_load/n_proc amout of work.
+This is also the configuration that minimze the idle time between processor 
+waiting for the other to finish their computations. 
 */
 
 #include "advection.h"
 #include "vof.h"
+
+/**
+Diagnostic functions needed for intresting statistics.
+'balance_score' computes the number of cells and the load assigned to 
+each processor.
+'parallel_efficency' gives a quantitative measure of the imbalance.
+*/
+void balance_score (long* counter, double* load, (const) scalar w) {
+  for (int ne = 0; ne < npe(); ne++) {
+    counter[ne] = 0;
+    load[ne] = 0.;
+  }
+
+  foreach() {
+    counter[pid()] += 1;
+    load[pid()] += w[];
+  }
+
+  if (pid() == 0) {
+    MPI_Reduce(MPI_IN_PLACE, counter, npe(), MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(MPI_IN_PLACE, load, npe(), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  } else {
+    MPI_Reduce(counter, NULL, npe(), MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(load, NULL, npe(), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  }
+}
+
+double parallel_efficency (double* load) {
+  //compute parallel efficency given the load array
+  double tot_load = 0., max_load = -1;
+  for (int ne = 0; ne < npe(); ne++) {
+    tot_load += load[ne];
+    if (load[ne] > max_load)
+      max_load = load[ne];
+  }
+  return (tot_load/npe())/max_load;
+}
 
 scalar c[];
 scalar * interfaces = {c}, * tracers = NULL;
@@ -19,21 +72,14 @@ int main() {
 
 event init (i = 0) {
   fraction (c, circle(x,y));
+  weights = new scalar;
 }
 
 #define end 0.785398
 
-scalar test[];
 event velocity (i++) {
-
- // double w_min;
- // if (weights.i > 0)
- //   w_min = statsf(weights).min;
- // else
- //   w_min = 1.;
-
- // foreach()
- //   test[] = w_min > 0 ? weights[]/w_min : 0.;
+  foreach()
+    weights[] = c[]*20. + 1.;
 
 #if TREE
   double cmax = 1e-3;
@@ -61,9 +107,8 @@ event logfile (i++) {
 
 /**
 We simulate a hefty computational cost in the cell with a non-zero
-volume fraction
+volume fraction.
 */
-
 event slowdown (i++) {
   foreach()
     if (c[] > 1e-10) {
@@ -93,10 +138,11 @@ set ylabel "Parallel efficiency"
 
 set yrange [0:1]
 
-plot "log" u 1:2 w l lw 3 notitle
+plot "log" u 1:2 w l lw 3 lc "red" notitle
 ~~~
 ~~~gnuplot cells per processor (stacked)
 
+set title "Realtive cell share in each processor"
 set xlabel "Time"
 set ylabel "% cells"
 set yrange [0:100]
@@ -113,6 +159,7 @@ plot for [i=7:3:-1] "log" u 1:(100.*(sum [c=3:i] column(c))/(sum [c=3:7] column(
 ~~~
 ~~~gnuplot load per processor (stacked)
 
+set title "Realtive load share in each processor"
 set xlabel "Time"
 set ylabel "% load"
 set yrange [0:100]
